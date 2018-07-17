78a29795dbd281f96e53441b4912e2690e5d3e69
Benjamin Thomann <benjamin.thomann@outlook.com>
2018-07-17 17:02:08 +0200

    Tested Regex

--- TestGIt/Program.cs
+++ TestGIt/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace TestGIt
 {
@@ -13,6 +10,41 @@ namespace TestGIt
             Console.WriteLine("Hello World");
 
             Console.WriteLine("Hallo wie geht es dir.");
+
+            string test = $"12.25 Watt";
+            Console.WriteLine(ParseString(test) + 0.25);
+
+            Console.ReadLine();
+        }
+
+        private static double ParseString(string name)
+        {
+            double value;
+            name = name.Trim();
+            name = name.Replace('.', ',');
+            name = Regex.Replace(name, "[^0-9.,]", "");
+            if( string.IsNullOrWhiteSpace(name) )
+            {
+                value = 0;
+            }
+            else
+            {
+                try
+                {
+                    value = Convert.ToDouble(name);
+                }
+                catch
+                {
+                    value = 0;
+                }
+
+                if( value < 0 )
+                {
+                    value = 0;
+                }
+            }
+
+            return value;
         }
     }
 }
